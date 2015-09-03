fa9cd55348076a649502950904425710fd0d5f93
Morten <morteano@stud.ntnu.no>
2015-09-03 13:39:12 +0200

    added domains to csp

--- Module2/InputModule2.py
+++ Module2/InputModule2.py
@@ -1,3 +1,5 @@
+K = 4
+
 from graphics import *
 
 class Node:
@@ -13,7 +15,7 @@ class CSP:
         self.variables = []
 
         # self.domains[i] is a list of legal values for variable i
-        self.domains = {}
+        self.domains = []
 
         # self.constraints[i][j] is a list of legal value pairs for
         # the variable pair (i, j)
@@ -32,17 +34,23 @@ def create_csp(filename):
     NV = int(firstLine[0]) # Number of vertices
     NE = int(firstLine[1]) # Number of edges
 
+    # Add nodes to csp.variables
     for i in range(NV):
         line = file.readline().split(" ")
         node = Node(int(line[0]), float(line[1]), float(line[2]))
         csp.variables.append(node)
 
-    for j in range(NE-1):
+    # Add neighbours to csp.constraints
+    for i in range(NE-1):
         line = file.readline().split(" ")
         nodeNr0 = int(line[0])
         nodeNr1 = int(line[1])
         csp.constraints[nodeNr0] = csp.variables[nodeNr1]
         csp.constraints[nodeNr1] = csp.variables[nodeNr0]
+
+    # Add K elements as domain of the nodes
+    for i in range(NV):
+        csp.domains.append(range(K))
 
     return csp
 
