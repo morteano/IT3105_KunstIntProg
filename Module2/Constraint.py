76f77be23c4455ec07b65911bfa6fe883d1a9b96
baisinthehole <helmer.n@hotmail.com>
2015-09-06 13:06:01 +0200

    completed REVISE*. Made a good deal of changes to other functions though...

---
+++ Module2/Constraint.py
@@ -0,0 +1,20 @@
+class Constraint:
+
+
+    #create constraint on the following form:
+
+    #varList contains all nodes that are involved in the constraint
+
+    #expression describes how the constraint is structured, for instance (n1 != n2) or (n1 < n2 + n3)
+    def __init__(self, varList, expression):
+        self.variables = varList
+        self.expression = expression
+
+
+
+
+
+
+
+
+
--- Module2/InputModule2.py
+++ Module2/InputModule2.py
@@ -1,9 +1,10 @@
 from Queue import Queue
 
-K = 4
+K = 6
 colors = {0: "blue", 1: "red", 2: "green", 3: "yellow", 4: "purple", 5: "orange"}
 
 from graphics import *
+from Constraint import *
 
 
 class Node:
@@ -12,6 +13,9 @@ class Node:
         self.xPos = xPos
         self.yPos = yPos
 
+        #textual representation of node, used for the general makefunc function
+        self.text = 'n' + str(nr)
+
 
 class CSP:
     def __init__(self):
@@ -19,15 +23,42 @@ class CSP:
         self.variables = []
 
         # self.domains[i] is a list of legal values for variable i
-        self.domains = []
+        self.domains = {}
 
         # self.constraints[i][j] is a list of legal value pairs for
         # the variable pair (i, j)
         self.constraints = {}
 
-    def revise(self):
-        magic = True
+    def printConstraints(self, variable):
+        for i in self.constraints[variable]:
+            print(i.expression)
 
+
+    # func = makefunc(['x', 'y', 'z'], 'x + y < 2*z')
+    def makefunc(self, varNames, expression, envir=globals()):
+        args = ""
+        for n in varNames: args = args + "," + n
+        print(eval("(lambda " + args[1:] + ": " + expression + ")", envir))
+        return eval("(lambda " + args[1:] + ": " + expression + ")", envir)
+
+    #not general yet, because it only works for constraints involving two variables,
+    #need one extra for loop for each additional variable
+    def revise(self, variable):
+        for i in self.constraints[variable]:
+            variableTexts = []
+            variablesInvolved = i.variables
+            for j in variablesInvolved:
+                variableTexts.append(j.text)
+
+            func = self.makefunc(variableTexts, i.expression)
+
+            if len(variablesInvolved) == 2:
+                for j in self.domains[variablesInvolved[0]]:
+                    for k in self.domains[variablesInvolved[1]]:
+                        if func(j, k):
+                            break
+                    else:
+                        self.domains[variable].remove(j)
 
 
 
@@ -49,29 +80,35 @@ def create_csp(filename):
         node = Node(int(line[0]), float(line[1]), float(line[2]))
         csp.variables.append(node)
         # Insert lists to the constraints dictionary
-        csp.constraints[i] = []
+        csp.constraints[node] = []
 
     # Add neighbours to csp.constraints
     for i in range(NE):
         line = file.readline().split(" ")
-        nodeNr0 = int(line[0])
-        nodeNr1 = int(line[1])
-        csp.constraints[nodeNr0].append(csp.variables[nodeNr1])
-        csp.constraints[nodeNr1].append(csp.variables[nodeNr0])
+
+        #nodeNr0 is the node in variables with id = line[0], same with nodeNr1
+        nodeNr0 = csp.variables[int(line[0])]
+        nodeNr1 = csp.variables[int(line[1])]
+
+        #add constraint instance for nodeNr0 where NodeNr0 and nodeNr1 are involved and the last argument is
+        #the constraint expression between the variables
+        csp.constraints[nodeNr0].append(Constraint([nodeNr0, nodeNr1], nodeNr0.text + ' != ' + nodeNr1.text))
+        csp.constraints[nodeNr1].append(Constraint([nodeNr1, nodeNr0], nodeNr1.text + ' != ' + nodeNr0.text))
 
     # Add K elements as domain of the nodes
-    for i in range(NV):
-        csp.domains.append(range(K))
-
+    for i in csp.variables:
+        csp.domains[i] = []
+        for j in range(K):
+            csp.domains[i].append(colors[j])
     return csp
 
 
 def displayGraph(csp):
-    win = GraphWin("CSP", 500, 500)
+    win = GraphWin("CSP", 600, 600)
     for i in csp.variables:
         # Draw edges
-        for neighbour in csp.constraints[i.id]:
-            line = Line(Point(i.xPos * 20, i.yPos * 20), Point(neighbour.xPos * 20, neighbour.yPos * 20))
+        for neighbour in csp.constraints[i]:
+            line = Line(Point(i.xPos * 20, i.yPos * 20), Point(neighbour.variables[1].xPos * 20, neighbour.variables[1].yPos * 20))
             line.draw(win)
         # Draw nodes
         circle = Circle(Point(i.xPos * 20, i.yPos * 20), 5)
@@ -80,13 +117,6 @@ def displayGraph(csp):
 
     win.getMouse()
     win.close()
-
-
-# func = makefunc(['x', 'y', 'z'], 'x + y < 2*z')
-def makefunc(var_names, expression, envir=globals()):
-    args = ""
-    for n in var_names: args = args + "," + n
-    return eval("(lambda " + args[1:] + ": " + expression + ")", envir)
 
 
 # Finds the smallest domain
@@ -99,8 +129,29 @@ def bestChoice(csp):
             var = i
     return var
 
+csp = create_csp("graphTest.txt")
+#print(csp.variables)
+#print(csp.domains)
 
-func = makefunc(['x', 'y'], 'x < y')
-csp = create_csp("graph1")
-print(bestChoice(csp))
+for i in csp.variables:
+    print(csp.domains[i])
+
+csp.domains[csp.variables[1]] = ['blue']
+csp.domains[csp.variables[2]] = ['red']
+
+csp.revise(csp.variables[0])
+
+for i in csp.variables:
+    print(csp.domains[i])
+
 displayGraph(csp)
+
+
+
+
+
+
+
+
+
+
