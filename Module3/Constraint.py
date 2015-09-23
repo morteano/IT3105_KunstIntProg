e5fd0158750b5c4edd83d0c4d35aaba1232390c8
Morten <morteano@stud.ntnu.no>
2015-09-23 16:35:15 +0200

    Added constraint class

---
+++ Module3/Constraint.py
@@ -0,0 +1,25 @@
+class Constraint:
+
+
+    #create constraint on the following form:
+
+    #varList contains all nodes that are involved in the constraint
+
+    #expression describes how the constraint is structured, for instance (n1 != n2) or (n1 < n2 + n3)
+    def __init__(self, varList, expression):
+        self.variables = varList
+        self.expression = expression
+
+    #check if variable is involved in constraint
+    def contains(self, variable):
+        if variable in self.variables:
+            return True
+        return False
+
+
+
+
+
+
+
+
