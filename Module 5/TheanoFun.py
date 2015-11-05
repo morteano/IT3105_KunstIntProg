cef5f1f6ebb9849d6f832db99db32d57481105dd
Morten <morteano@stud.ntnu.no>
2015-11-05 12:34:59 +0100

    Added LogisticRegression

--- Module 5/TheanoFun.py
+++ Module 5/TheanoFun.py
@@ -1,6 +1,9 @@
 from basics.mnist_basics import *
+from LogisticRegression import *
 
 import numpy
+import theano
+import theano.tensor as T
 
 helmerPath = "/Users/Butikk/.PyCharm40/AI 5/IT3105_KunstIntProg/Module 5/basics/"
 
@@ -8,4 +11,11 @@ case = load_cases("demo_prep", helmerPath, True)
 
 images, labels = load_mnist("training", numpy.arange(10), helmerPath)
 
-quicktest(1)
+# generate symbolic variables for input (x and y represent a
+# minibatch)
+x = T.matrix('x')  # data, presented as rasterized images
+y = T.ivector('y')  # labels, presented as 1D vector of [int] labels
+
+# construct the logistic regression class
+# Each MNIST image has size 28*28
+classifier = LogisticRegression(input=x, n_in=28 * 28, n_out=10)
