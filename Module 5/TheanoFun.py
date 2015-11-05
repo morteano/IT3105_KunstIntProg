3171f6c442ff5373518dce2542c8b8d603be7814
baisinthehole <helmer.n@hotmail.com>
2015-11-05 10:43:57 +0100

    all necessary files

--- Module 5/TheanoFun.py
+++ Module 5/TheanoFun.py
@@ -1,1 +1,11 @@
+from basics.mnist_basics import *
 
+import numpy
+
+helmerPath = "/Users/Butikk/.PyCharm40/AI 5/IT3105_KunstIntProg/Module 5/basics/"
+
+case = load_cases("demo_prep", helmerPath, True)
+
+images, labels = load_mnist("training", numpy.arange(10), helmerPath)
+
+quicktest(1)
