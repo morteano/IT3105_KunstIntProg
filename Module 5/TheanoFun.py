1bb3d3ff66edcdf894346554ed17d81ec3828ffe
baisinthehole <helmer.n@hotmail.com>
2015-11-15 20:11:40 +0100

    yay

--- Module 5/TheanoFun.py
+++ Module 5/TheanoFun.py
@@ -7,15 +7,24 @@ import theano.tensor as T
 import theano.tensor.nnet as Tann
 import matplotlib.pyplot as plt
 
+import time
+
 def genallbitcases(numbits):
     def bits(n):
         s = bin(n)[2:]
         return [int(b) for b in '0' *(numbits - len(s)) + s]
     return [ bits(i) for i in range(2**numbits)]
 
+def createCompVector(label):
+    vector = []
+    for i in range(10):
+        vector.append(0)
+    vector[label] = 1
+    return vector
+
 class autoencoder:
-    def __init__(self, images, labels, nb=10, nh=100, nob=10, lr=.1):
-        self.cases = genallbitcases(nb)
+    def __init__(self, images, labels, nb=28**2, nh=100, nob=10, lr=.1):
+        self.cases = images
         self.labels = labels
         self.lrate = lr
         self.buildann(nb, nh, nob, lr)
@@ -24,40 +33,68 @@ class autoencoder:
         w1 = theano.shared(np.random.uniform(-.1, .1, size = (nb, nh)))
         w2 = theano.shared(np.random.uniform(-.1, .1, size = (nh, nob)))
         input = T.dvector ('input')
-        label  = T.ivector ('label')
+        label = T.dvector ('label')
         b1 = theano.shared(np.random.uniform(-.1, .1, size = nh))
         b2 = theano.shared(np.random.uniform(-.1, .1, size = nob))
         x1 = Tann.sigmoid(T.dot(input, w1) + b1)
         x2 = Tann.sigmoid(T.dot(x1, w2) + b2)
-        error = 0 if maxIndex(x2) == label else 1
+        error = T.sum((x2 - label)**2)
         params = [w1, b1, w2, b2]
         gradients = T.grad(error, params)
         backprop_acts = [(p, p - self.lrate * g) for p,g in zip(params, gradients)]
-        self.predictor = theano.function([input], [x2, x1])
-        self.trainer = theano.function([label], error, updates = backprop_acts)
+        self.predictor = theano.function([input, label], [x2])
+        self.trainer = theano.function([input, label], [x2, error], updates = backprop_acts)
 
     def dotraining(self, epochs = 100):
         errors = []
         for i in range(epochs):
-            error = 0
+            mistakes = 0
+            totalError = 0
+            j = 0
+            start = time.time()
             for c in self.cases:
-                error += self.trainer(c)
-            errors.append(error)
+                x2, error = self.trainer(c, createCompVector(labels[j]))
+                maxValue = x2[0]
+                maxIndex = 0
+                for k in range(10):
+                    if maxValue < x2[k]:
+                        maxValue = x2[k]
+                        maxIndex = k
+                if maxIndex != labels[j][0]:
+                    mistakes += 1
+                totalError += error
+                j += 1
+            end = time.time()
+            errors.append(totalError)
+            print("Error", totalError)
+            print("Amount of mistakes", mistakes)
+            print("Time", end - start)
         return errors
 
     def dotesting(self):
-        hidden_activations = []
+        mistakeList = []
         for c in self.cases:
-            hact = self.predictor(c)
-            hidden_activations.append(hact)
-        return hidden_activations
+            mistakes = 0
+            j = 0
+            x2 = self.predictor(c)
+            maxValue = x2[0]
+            maxIndex = 0
+            for k in range(10):
+                if maxValue < x2[k]:
+                    maxValue = x2[k]
+                    maxIndex = k
+            if maxIndex != labels[j][0]:
+                mistakes += 1
+            j += 1
+            mistakeList.append(mistakes)
+        return mistakeList
 
 helmerPath = "/Users/Butikk/.PyCharm40/AI 5/IT3105_KunstIntProg/Module 5/basics/"
 normannPath = "/Users/MortenAlver/PycharmProjects/IT3105_KunstIntProg/Module 5/basics/"
 
-case = load_cases("demo_prep", normannPath, True)
+case = load_cases("demo_prep", helmerPath, True)
 
-images, labels = load_mnist("training", np.arange(10), normannPath)
+images, labels = load_mnist("training", np.arange(10), helmerPath)
 
 def maxIndex(x2):
     return 2
@@ -82,9 +119,7 @@ def getFlatInput(images):
         flatImages.append(flatten_image(image))
     return flatImages
 
-
 flatImages = getFlatInput(images)
-print("1")
 auto = autoencoder(flatImages, labels)
 error = auto.dotraining(100)
 epochs = []
@@ -98,6 +133,5 @@ plt.ylabel('error')
 plt.title('Result')
 plt.show()
 
-print(hidden)
 plt.plot(*zip(*hidden), marker='o', color='r', ls='')
 plt.show()
