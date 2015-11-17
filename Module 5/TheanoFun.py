f32662d8c0ab902de8f9035e0c7b4372c7b52ced
baisinthehole <helmer.n@hotmail.com>
2015-11-17 19:13:23 +0100

    load and save

--- Module 5/TheanoFun.py
+++ Module 5/TheanoFun.py
@@ -8,15 +8,10 @@ import theano.tensor.nnet as Tann
 import matplotlib.pyplot as plt
 
 import time
+import pickle
 
 helmerPath = "/Users/Butikk/.PyCharm40/AI 5/IT3105_KunstIntProg/Module 5/basics/"
 normannPath = "/Users/MortenAlver/PycharmProjects/IT3105_KunstIntProg/Module 5/basics/"
-
-def genallbitcases(numbits):
-    def bits(n):
-        s = bin(n)[2:]
-        return [int(b) for b in '0' *(numbits - len(s)) + s]
-    return [ bits(i) for i in range(2**numbits)]
 
 def createCompVector(label):
     vector = []
@@ -25,55 +20,85 @@ def createCompVector(label):
     vector[label] = 1
     return vector
 
+def relu(x):
+    return theano.tensor.switch(x<0, 0, x)
+
 class ann:
-    def __init__(self, images, labels, ni=28**2, nh=100, no=10, lr=.1):
+    #initiate values, and start building the neural network.
+    #since there can be several layers of hidden nodes, nh is an array with amount of nodes in the corresponding layer
+    def __init__(self, images, labels, ni=28**2, nh=[100], no=10, lr=.1):
         self.cases = images
         self.labels = labels
         self.numInputNodes = ni
         self.numHiddenNodes = nh
         self.numOutputNodes = no
         self.lrate = lr
+        self.w = []
+        self.b = []
         self.buildann(ni, nh, no, lr)
 
+    #build the neural network
     def buildann(self, nb , nh, nob, lr):
-        w = []
-        b = []
         x = []
+
+        #weights with initial random values between -0.1 and 0.1
         for i in range(len(nh)):
             if i == 0:
-                w.append(theano.shared(np.random.uniform(-.1, .1, size = (nb, nh[i]))))
+                self.w.append(theano.shared(np.random.uniform(-.1, .1, size = (nb, nh[i]))))
             if i != 0:
-                w.append(theano.shared(np.random.uniform(-.1, .1, size = (nh[i - 1], nh[i]))))
+                self.w.append(theano.shared(np.random.uniform(-.1, .1, size = (nh[i - 1], nh[i]))))
             if i == len(nh) - 1:
-                w.append(theano.shared(np.random.uniform(-.1, .1, size = (nh[i], nob))))
+                self.w.append(theano.shared(np.random.uniform(-.1, .1, size = (nh[i], nob))))
+
+        #input is the image, label is the possible answers(0 to 9)
         input = T.dvector ('input')
         label = T.dvector ('label')
+
+        #node values with initial random values between -0.1 and 0.1
         for i in range(len(nh)):
-            b.append(theano.shared(np.random.uniform(-.1, .1, size = nh[i])))
+            self.b.append(theano.shared(np.random.uniform(-.1, .1, size = nh[i])))
             if i == len(nh) - 1:
-                b.append(theano.shared(np.random.uniform(-.1, .1, size = nob)))
+                self.b.append(theano.shared(np.random.uniform(-.1, .1, size = nob)))
+
+        #activation functions
         for i in range(len(nh)):
             if i == 0:
-                x.append(Tann.sigmoid(T.dot(input, w[i]) + b[i]))
-            x.append(Tann.sigmoid(T.dot(x[i], w[i + 1]) + b[i + 1]))
+                x.append(Tann.hard_sigmoid(T.dot(input, self.w[i]) + self.b[i]))
+            x.append(Tann.hard_sigmoid(T.dot(x[i], self.w[i + 1]) + self.b[i + 1]))
+
+        #error calculation, which gives least error for right guesses
         error = T.sum((x[len(nh)] - label)**2)
+
+        #parameters needed for the gradient search
         params = []
-        for i in range(len(w)):
-            params.append(w[i])
-            params.append(b[i])
-        #params = [w[0], b[0], w[1], b[1]]
+        for i in range(len(self.w)):
+            params.append(self.w[i])
+            params.append(self.b[i])
+
+        #gradient search
         gradients = T.grad(error, params)
+
+        #backpropagation for updating weigths and node values
         backprop_acts = [(p, p - self.lrate * g) for p,g in zip(params, gradients)]
+
+        #testing function
         self.predictor = theano.function([input], x[len(nh)])
+
+        #training function
         self.trainer = theano.function([input, label], [x[len(nh)], error], updates = backprop_acts)
 
-    def dotraining(self, epochs = 100):
+    #train the neural network. epochs is amount of iterations of training
+    def dotraining(self, labels, epochs = 100):
         errors = []
         start = time.time()
+
+        #backpropagates for every epoch, to train the neural network on the current dataset
         for i in range(epochs):
             mistakes = 0
             totalError = 0
             j = 0
+
+            #checks if the guess for every case is right or wrong, and sums up the total amount of mistakes
             for c in self.cases:
                 xLast, error = self.trainer(c, createCompVector(labels[j]))
                 maxValue = xLast[0]
@@ -86,6 +111,7 @@ class ann:
                     mistakes += 1
                 totalError += error
                 j += 1
+
             errors.append(totalError)
             print("Error", totalError)
             print("Amount of mistakes", mistakes)
@@ -94,7 +120,7 @@ class ann:
         print("Total training time", end - start)
         return errors
 
-    def dotesting(self):
+    def dotesting(self, labels):
         mistakeList = []
         for c in self.cases:
             mistakes = 0
@@ -112,6 +138,7 @@ class ann:
             mistakeList.append(mistakes)
         return mistakeList
 
+    #test the neural network on a testing dataset and return a list of all guesses
     def blind_test(self, feature_sets):
         finalResult = []
         for c in feature_sets:
@@ -124,11 +151,6 @@ class ann:
                     maxIndex = k
             finalResult.append(maxIndex)
         return finalResult
-
-
-def maxIndex(x2):
-    return 2
-
 
 def filter(image):
     for j in image:
@@ -149,30 +171,54 @@ def getFlatInput(images):
         flatImages.append(flatten_image(image))
     return flatImages
 
-case = load_cases("demo_prep", helmerPath, True)
+def runTraining(ni = 28**2, nh = [100], no = 10, lr = .1):
+    #load the training dataset
+    images, labels = load_mnist("training", np.arange(10), helmerPath)
+    flatImages = getFlatInput(images)
 
-images, labels = load_mnist("training", np.arange(10), helmerPath)
+    #create and train the neural network
+    network = ann(flatImages, labels, ni, nh, no, lr)
+    error = network.dotraining(labels, 10)
 
-testImages, testLabels = load_mnist("testing", np.arange(10), helmerPath)
+    #save the trained network
+    file = open("trainedAnn", 'wb')
+    pickle.dump(network, file, protocol=pickle.HIGHEST_PROTOCOL)
+    file.close()
 
-flatTestImages = getFlatInput(testImages)
+    return error
 
-flatImages = getFlatInput(images)
-network = ann(flatImages, labels, 28**2, [50, 30], 10, .1)
-error = network.dotraining(100)
+def runTesting(dataset):
+    #load the trained network
+    file = open("trainedAnn", 'rb')
+    network = pickle.load(file)
+    file.close()
+
+    #load the testing dataset
+    testImages, testLabels = load_mnist(dataset, np.arange(10), helmerPath)
+
+    flatTestImages = getFlatInput(testImages)
+
+    #run the dataset through the trained neural network
+    result = network.blind_test(flatTestImages)
+
+    #compare result with right answers and find the correctness percentage
+    mistakes = 0
+    for i in range(len(result)):
+
+        if result[i] != testLabels[i]:
+            mistakes += 1
+
+    print("Test mistakes", mistakes)
+    print("Percentage", 1 - (mistakes / len(result)))
+
+#error = runTraining(28**2, [100], 10, .1)
+runTesting("training")
+runTesting("testing")
+#case = load_cases("demo_prep", helmerPath, True)
+
 """epochs = []
 for i in range(len(error)):
     epochs.append(i)"""
-result = network.blind_test(flatTestImages)
-
-mistakes = 0
-for i in range(len(result)):
-
-    if result[i] != testLabels[i]:
-        mistakes += 1
-
-print("Test mistakes", mistakes)
-print("Percentage", 1 - (mistakes / len(result)))
 
 
 
